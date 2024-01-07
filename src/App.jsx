85093a9978cb159eb6f02692fffe63e03da4640a
Sanjana Shivanand <sanjanashivananda07@gmail.com>
2024-01-07 16:14:10 -0700

    desktop-nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import { Toggle } from './components/Toggle';
+import useLocalStorage from 'use-local-storage';
+import { AboutMe } from './components/AboutMe';
+import { Blog } from './components/Blog/Blog';
+import{Route, Routes, Link, NavLink} from 'react-router-dom'
+import { Projects } from './components/Projects/Projects';
+import { About } from './components/About/About';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const[isDark, setIsDark] = useLocalStorage("isDark", false);
+
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div className='App' data-theme={isDark ? "dark" : "light"}>
+
+      <nav className='desktop-nav'>
+        <ul>
+          <li>
+            <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/blog'>Blog</NavLink>
+          </li>
+          <li>
+            <NavLink to='/projects'>Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to='/about'>About Me</NavLink>
+          </li>
+        </ul>
+        
+        <Toggle
+          isChecked={isDark}
+          handleChange={() => setIsDark(!isDark)}
+        />
+        
+      </nav>
+  
+      
+      
+      <Routes>
+        <Route path='*' element={<AboutMe/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+
+    </div>
   )
 }
-
-export default App
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AboutMe.css'
+
+export const AboutMe = () => {
+  return (
+    <div className='about-me'>
+        <h1 className='title'>Sanjana Shivananda</h1>
+        
+        <div>
+        <a href="../assests/SanjanaShivananda.pdf" download="../assests/SanjanaShivananda.pdf">
+            <button class="box">Download Resume</button>
+        </a>
+        </div>
+
+    </div>
+  )
+}
