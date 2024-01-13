9d4852fb05b7ec680b1dc937af6015824a9993a2
Sanjana Shivanand <sanjanashivananda07@gmail.com>
2024-01-12 22:20:38 -0700

    project-info

--- src/components/Projects/ProjectInfo/ProjectInfo.jsx
+++ src/components/Projects/ProjectInfo/ProjectInfo.jsx
@@ -1,21 +1,56 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
+import ProjectDetails from '../ProjectDetails';
+import './ProjectInfo.css'
+import { HiMiniCommandLine } from "react-icons/hi2";
 
 
 export const ProjectInfo = () => {
     const { id } = useParams();
-    const selectedProject = ProjectDetails.find((project) => project.id === id);
+    const [project, setProject] = useState(null); // Initialize state with null
   
+    const findProjectById = (projectId) => {
+      return ProjectDetails.find(proj => proj.id === projectId);
+    };
+
+
+  
+    useEffect(() => {
+      const fetchProject = () => {
+        const foundProject = findProjectById(id);
+        setProject(foundProject);
+      };
+  
+      fetchProject();
+    }, [id]); 
+
+    useEffect(() => {
+        if (project) {
+          // Perform any actions with the found project here
+          console.log('Found project:', project);
+        }
+      }, [project]);
+
+    
+
     return (
-      <div>
-        {selectedProject ? (
-          <>
-            <h1>{selectedProject.title}</h1>
-            <p>{selectedProject.description}</p>
-          </>
-        ) : (
-          <p>No project found for ID: {id}</p>
-        )}
+      <div className='project-info-container'>
+        {!project ? <h1>Loading</h1> :
+            <div className='project-info'>
+            <div className='project-header'>
+              <h1 className='project-heading'>
+                {project.title}
+              </h1>
+              <span className='view-code-icon'>
+                  <a href={project.code}>
+                    <HiMiniCommandLine /> View Code
+                  </a>
+                </span>
+            </div>
+            <img className='cover-image' src={project.image} alt={project.title} />
+            <div className='content' dangerouslySetInnerHTML={{ __html: project.description }} />
+          </div>
+        }
       </div>
     );
 }
